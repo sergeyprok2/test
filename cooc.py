59aafea66cd2a96dcee99287a371e1b7bae30ace
sergeyprok2 <124092599+sergeyprok2@users.noreply.github.com>
2024-02-07 23:28:44 +0700

    4

---
+++ cooc.py
@@ -0,0 +1,88 @@
+cookies_bs4 = {
+    'remixlang': '0',
+    'remixstlid': '9050927473257781776_ub2MfezA0ozT0qbfyFjEosgTzECkOrU1VTp4gK77xAw',
+    'remixstid': '1632729085_gedOhdCABj65u3KeFYzPyTam7ENzmceccNcHQZtE1ek',
+    'remixcolor_scheme_mode': 'auto',
+    'tmr_lvid': 'd9c6c3454a4f650e14c19def379eda59',
+    'tmr_lvidTS': '1683048329136',
+    'remixdark_color_scheme': '1',
+    'remixmdv': 'gERJ3IisqCkOB5iW',
+    'remixff': '10111111111111',
+    'remixdt': '14400',
+    'remixgp': '20f9b2a939d1e36b42481b6177e2c319',
+    'remixmvk-fp': '3c8fcb9e85c0e9df1579ff13558889ee',
+    'tmr_detect': '1%7C1695923443719',
+    'earlyhints': '1',
+    'remixcurr_audio': 'undefined',
+    'remixua': '15%7C247%7C202%7C3083430651',
+    'remixmdevice': '377/1193/2/!!!!!!!!!!!-/377',
+}
+
+headers_bs4 = {
+    'authority': 'm.vk.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'cache-control': 'max-age=0',
+    # 'cookie': 'remixlang=0; remixstlid=9050927473257781776_ub2MfezA0ozT0qbfyFjEosgTzECkOrU1VTp4gK77xAw; remixstid=1632729085_gedOhdCABj65u3KeFYzPyTam7ENzmceccNcHQZtE1ek; remixcolor_scheme_mode=auto; tmr_lvid=d9c6c3454a4f650e14c19def379eda59; tmr_lvidTS=1683048329136; remixdark_color_scheme=1; remixmdv=gERJ3IisqCkOB5iW; remixff=10111111111111; remixdt=14400; remixgp=20f9b2a939d1e36b42481b6177e2c319; remixmvk-fp=3c8fcb9e85c0e9df1579ff13558889ee; tmr_detect=1%7C1695923443719; earlyhints=1; remixcurr_audio=undefined; remixua=15%7C247%7C202%7C3083430651; remixmdevice=377/1193/2/!!!!!!!!!!!-/377',
+    'referer': 'https://m.vk.com/jobprk',
+    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+}
+
+
+
+cookies_ya = {
+    'yandex_login': '',
+    'yandexuid': '7986167821683047642',
+    '_ym_uid': '1686945243823701731',
+    '_ym_d': '1686945243',
+    'tmr_lvid': '6dfaf88cc5fc7a63da810b3638975cea',
+    'tmr_lvidTS': '1686945243391',
+    'vid': '873af10575d61ad3',
+    'zen_gid': '11291',
+    'KIykI': '1',
+    'addruid': 'n16R9TX4T96GB46RQ73l0Gk8r0',
+    'crookie': 'vg6vCy2sSLN3IguJi3hebBUryljWqn3XikbJgbKXylrP2zjdkjWZabI07McqbDa6pDmKVcxGxpvfpteJnjH/gW7Jg/4=',
+    'cmtchd': 'MTY5NTA1Mzc5ODU3OQ==',
+    'zen_sso_checked': '1',
+    'Session_id': 'noauth:1695491008',
+    'sessar': '1.1182.CiCTtdFAQ4KxcrqqRuU5OfTxPEx2NxOTPS0JmyNRfcUDHw.m6l8ornGoaYDXpT0QK4hRDMmGFMHEjuGMq-Io0ayrM8',
+    'ys': 'c_chck.1308660127',
+    'mda2_beacon': '1695491008335',
+    '_ym_isad': '1',
+    'Zen-User-Data': '{%22zen-theme%22:%22light%22}',
+    'ask_city': '+',
+    'tmr_detect': '1%7C1695496006520',
+    '_yasc': '0UuriHacVByJsdceSZ4O1Y++vvsW7Vmfv+MBQs80sAa1WxFv8oiPTbmOXzFbXBxdKiZkYQ==',
+}
+
+headers_ya = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'yandex_login=; yandexuid=7986167821683047642; _ym_uid=1686945243823701731; _ym_d=1686945243; tmr_lvid=6dfaf88cc5fc7a63da810b3638975cea; tmr_lvidTS=1686945243391; vid=873af10575d61ad3; zen_gid=11291; KIykI=1; addruid=n16R9TX4T96GB46RQ73l0Gk8r0; crookie=vg6vCy2sSLN3IguJi3hebBUryljWqn3XikbJgbKXylrP2zjdkjWZabI07McqbDa6pDmKVcxGxpvfpteJnjH/gW7Jg/4=; cmtchd=MTY5NTA1Mzc5ODU3OQ==; zen_sso_checked=1; Session_id=noauth:1695491008; sessar=1.1182.CiCTtdFAQ4KxcrqqRuU5OfTxPEx2NxOTPS0JmyNRfcUDHw.m6l8ornGoaYDXpT0QK4hRDMmGFMHEjuGMq-Io0ayrM8; ys=c_chck.1308660127; mda2_beacon=1695491008335; _ym_isad=1; Zen-User-Data={%22zen-theme%22:%22light%22}; ask_city=+; tmr_detect=1%7C1695496006520; _yasc=0UuriHacVByJsdceSZ4O1Y++vvsW7Vmfv+MBQs80sAa1WxFv8oiPTbmOXzFbXBxdKiZkYQ==',
+    'Referer': 'https://m.dzen.ru/news/story/RIA_Novosti_razvedchiki_unichtozhili_tank_Leopard_sehkipazhem_armii_FRG--a161111dcc1f1326047292a0c367568d?lang=ru&from=main_portal&fan=1&stid=STEwBuJff9DUvftYhaMr&t=1695493665&persistent_id=4666199950789852023&tst=1695494051&story=37ca3162-a963-5720-91c2-510fa300c030&issue_tld=ru&contour=exp0&utm_referrer=m.dzen.ru',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+}
+
+host1 = '127.0.0.1'
+user1 ="root"
+passwd1 ="qwerty1007"
+database1 ="baza"
+
+bitly='29fe9a4477cdc72962901b677ae531f2b6d07546'
